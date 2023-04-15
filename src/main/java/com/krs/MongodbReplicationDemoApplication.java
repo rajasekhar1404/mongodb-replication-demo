492175755e240268f08500ae00f3932f38a7c3c1
rajasekhar1404 <mrrajasekhar09@gmail.com>
2023-04-15 17:28:40 +0530

    Initial commit

---
+++ src/main/java/com/krs/MongodbReplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.krs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbReplicationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbReplicationDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/krs/configuration/MongoConfig.java
@@ -0,0 +1,2 @@
+package com.krs.configuration;public class MongoConfig {
+}
